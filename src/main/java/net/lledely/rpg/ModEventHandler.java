fcdc76f44b9843c59bd00ea91b3df2613c116233
Lledely <71928776+Lledely@users.noreply.github.com>
2024-06-13 11:43:43 +0300

    Update ModEventHandler.java

--- src/main/java/net/lledely/rpg/ModEventHandler.java
+++ src/main/java/net/lledely/rpg/ModEventHandler.java
@@ -1,16 +1,25 @@
 package net.lledely.rpg;
 
+import net.lledely.rpg.effect.RefractionEffect;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Iterator;
+
 @Mod.EventBusSubscriber(Dist.CLIENT)
 public class ModEventHandler {
 
     @SubscribeEvent
-    public void hurtEvent(LivingHurtEvent event) {
-
+    public static void hurtEvent(LivingHurtEvent event) {
+        java.util.Collection<net.minecraft.world.effect.MobEffectInstance> activeEffects = event.getEntity().getActiveEffects();
+        for (MobEffectInstance effect : activeEffects) {
+            if (effect.getEffect() instanceof RefractionEffect) {
+                event.setAmount(0);
+            }
+        }
     }
 
 }
